f42cbd403a4dd48efb61cc139c4238d6d7cf1adf
rac3rx <chris.hammond.tx@gmail.com>
2020-02-20 08:59:19 -0600

    tweak

--- install.sh
+++ install.sh
@@ -21,19 +21,19 @@
 #              SETUP
 #      [x]         BASHRC $PATH STATEMENTS - HOMEBREW FOR LINUX
 #      [x]             HOMEBREW FOR LINUX
-#      [x]                 linuxbrew/bin: Warning: /home/linuxbrew/.linuxbrew/bin is not in your PATH.
+#      [x]                 linuxbrew/bin: Warning: $HOMEBREW_PREFIX/bin is not in your PATH.
 #      [x]                 Configure Homebrew in your ~/.bash_profile by running
-#      [x]                     echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.bash_profile
+#      [x]                     echo 'eval $($HOMEBREW_PREFIX/bin/brew shellenv)' >>~/.bash_profile
 #      [x]             RUBY
 #      [x]                 Add Ruby to your PATH by running:
-#      [x]                 PATH=/home/linuxbrew/.linuxbrew/Homebrew/Library/Homebrew/vendor/portable-ruby/current/bin:$PATH
+#      [x]                 PATH=$HOMEBREW_PREFIX/Homebrew/Library/Homebrew/vendor/portable-ruby/current/bin:$PATH
 #      [ ]             GCC/Compilers
 #      [ ]                 For compilers to find libnsl you may need to set:
-#      [ ]                     export LDFLAGS="-L/home/linuxbrew/.linuxbrew/opt/libnsl/lib"
-#      [ ]                     export CPPFLAGS="-I/home/linuxbrew/.linuxbrew/opt/libnsl/include"
+#      [ ]                     export LDFLAGS="-L$HOMEBREW_PREFIX/opt/libnsl/lib"
+#      [ ]                     export CPPFLAGS="-I$HOMEBREW_PREFIX/opt/libnsl/include"
 #      [ ]
 #      [ ]                 For pkg-config to find libnsl you may need to set:
-#      [ ]                   export PKG_CONFIG_PATH="/home/linuxbrew/.linuxbrew/opt/libnsl/lib/pkgconfig"
+#      [ ]                   export PKG_CONFIG_PATH="$HOMEBREW_PREFIX/opt/libnsl/lib/pkgconfig"
 #      [ ]             ZSH
 #      [ ]                 export ZSH=${HOME}/.oh-my-zsh
 #                      vtop alias
@@ -43,18 +43,18 @@
 #                      tmux
 #                          ???line264:~/.tmux/plugins/tpm/scripts/install_plugins.sh???
 #      [ ]         ZSHRC
-#      [ ]             . /home/linuxbrew/.linuxbrew/etc/profile.d/z.sh  <--bashrc|zshrc
+#      [ ]             . $HOMEBREW_PREFIX/etc/profile.d/z.sh  <--bashrc|zshrc
 #      [ ]         NVIM dot files (init.vim)
 #      [ ]             cp -R ./config/nvim/* ~/.config/nvim/
 #      [ ] FZF
-#      [ ]     set rtp+=/home/linuxbrew/.linuxbrew/opt/fzf
+#      [ ]     set rtp+=$HOMEBREW_PREFIX/opt/fzf
 #      [ ]     To install useful keybindings and fuzzy completion:
-#      [ ]     /home/linuxbrew/.linuxbrew/opt/fzf/install
+#      [ ]     $HOMEBREW_PREFIX/opt/fzf/install
 #      [ ] ERRORS
 #      [ ]     airline: Could not resolve airline theme "space". Themes have been migrated to
 #                   github.com/vim-airline/vim-airline-themes.
 #      [ ]     spaceship
-#      [ ]         SPACESHIP: Failed to symlink /home/linuxbrew/.linuxbrew/lib/node_modules/spaceship-prompt/spaceship.zsh to
+#      [ ]         SPACESHIP: Failed to symlink $HOMEBREW_PREFIX/lib/node_modules/spaceship-prompt/spaceship.zsh to
 #                             /usr/local/share/zsh/site-functions.
 #      [ ]     tmux
 #      [ ]         unknown variable: TMUX_PLUGIN_MANAGER_PATH
@@ -80,7 +80,7 @@ INSTALLDIR=$PWD
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2)JARVIS: Checking for Homebrew installation.$(tput sgr 0)"
 echo "---------------------------------------------------------"
-brew="/home/linuxbrew/.linuxbrew/bin/brew"
+brew="$HOMEBREW_PREFIX/bin/brew"
 yum="/usr/bin/yum"
 
 if [ -f "$brew" ]
@@ -93,8 +93,9 @@ else
     echo "$(tput setaf 3)JARVIS: Installing Homebrew. More applications available under sudo equivalent group$(tput sgr 0)"
     echo "------------------------------------------------------------------------------------------------------"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-    export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
-    export PATH=/home/linuxbrew/.linuxbrew/Homebrew/Library/Homebrew/vendor/portable-ruby/current/bin:$PATH
+    export PATH=$HOMEBREW_PREFIX/bin:$PATH
+    export PATH=$HOMEBREW_PREFIX/Homebrew/Library/Homebrew/vendor/portable-ruby/current/bin:$PATH
+    export PATH=$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/Homebrew/Library/Homebrew/vendor/portable-ruby/current/bin:$PATH
     if [ -f ${yum} ];
     then
         sudo yum group list development-tools | grep -q Installed; 
@@ -125,11 +126,15 @@ then
     cp ${HOME}/.bashrc ${HOME}/bashrc.$(date +%d%H%M%b%y)
     # HOMEBREW FOR LINUX - $PATH
     echo '# HOMEBREW FOR LINUX - $PATH' >> ${HOME}/.bashrc
-    echo 'PATH=/home/linuxbrew/.linuxbrew/bin:$PATH' >> ${HOME}/.bashrc
-    echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> ~/.bashrc
+    echo 'PATH=$HOMEBREW_PREFIX/bin:$PATH' >> ${HOME}/.bashrc
+    echo 'eval $($HOMEBREW_PREFIX/bin/brew shellenv)' >> ~/.bashrc
+    echo 'export MANPATH="/usr/local/man:$MANPATH"' >> ~/.bashrc
+    echo 'export PATH="$HOMEBREW_PREFIX/bin:$PATH"' >> ~/.bashrc
+    echo 'export MANPATH="$HOMEBREW_PREFIX/share/man:$MANPATH"' >> ~/.bashrc
+    echo 'export INFOPATH="$HOMEBREW_PREFIX/share/info:$INFOPATH"' >> ~/.bashrc
     # RUBY - HOMEBREW FOR LINUX - $PATH
     echo '# RUBY - HOMEBREW FOR LINUX - $PATH' >> ${HOME}/.bashrc
-    echo 'PATH=/home/linuxbrew/.linuxbrew/Homebrew/Library/Homebrew/vendor/portable-ruby/current/bin:$PATH' >> ${HOME}/.bashrc
+    echo 'PATH=$HOMEBREW_PREFIX/Homebrew/Library/Homebrew/vendor/portable-ruby/current/bin:$PATH' >> ${HOME}/.bashrc
     # OHMYZSH
     echo 'export ZSH=${HOME}/.oh-my-zsh' >> ${HOME}/.bashrc
     . ~/.bashrc
@@ -143,6 +148,7 @@ echo "---------------------------------------------------------"
 # echo 'eval "$(perl -I$HOME/perl6/lib/perl5 -Mlocal::lib=$HOME/perl5)"' >> ~/.bashrc
 
 packages=(
+  "bash"
   "fzf"
   "gcc"
   "git"
@@ -153,6 +159,7 @@ packages=(
   "ruby"
   "vim"
   "tmux"
+  "yarn"
   "zsh"
 )
 
@@ -211,7 +218,7 @@ cd ~/.local/share/fonts && curl -fLo "Hack Regular Nerd Font Complete" "https://
 fc-cache ~/.local/share/fonts    #bash: fc-cache: command not found
 #fc-list | grep local.share.font
 
-localGit="/home/linuxbrew/.linuxbrew/bin/git"
+localGit="$HOMEBREW_PREFIX/bin/git"
 if ! [[ -f "$localGit" ]]; then
   echo "---------------------------------------------------------"
   echo "$(tput setaf 1)JARVIS: Invalid git installation. Aborting. Please install git.$(tput sgr 0)"
@@ -254,13 +261,14 @@ source install/link.sh
 
 nvim +PlugInstall +qall
 nvim +UpdateRemotePlugins +qall
-nvim -c 'CocInstall -sync coc-css coc-emmet coc-json coc-html|q'
+nvim -c 'CocInstall -sync coc-css coc-emmet coc-eslint coc-git coc-highlight coc-html coc-java coc-json coc-marketplace coc-prettier coc-python coc-snippets coc-spell-checker coc-tsserver coc-ultisnips|q'
 nvim -c 'CocUpdateSync|q'
 
 
+mkdir ~/.cache/vim                                        # directory to store swap files
 vim +PlugInstall +qall
 vim +UpdateRemotePlugins +qall
-vim -c 'CocInstall -sync coc-css coc-emmet coc-java coc-json coc-html coc-snippets coc-tslint-plugin coc-tsserver coc-ultisnips|q'
+vim -c 'CocInstall -sync coc-css coc-emmet coc-eslint coc-git coc-highlight coc-html coc-java coc-json coc-marketplace coc-prettier coc-python coc-snippets coc-spell-checker coc-tsserver coc-ultisnips|q'
 vim -c 'CocUpdateSync|q'     # :call coc#util#install()    #fixed error
 
 echo "---------------------------------------------------------"
